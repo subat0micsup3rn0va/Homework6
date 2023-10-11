838dd54152c8d34896f8071a73f6037b26eaf4e6
Polina Kostsova <kostsovapa@gmail.com>
2023-10-12 01:38:36 +0300

    First attempt

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
 ﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int Prompt (string message)
+{
+    System.Console.Write (message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
