a0a7f50a2c4f8b0862760e3ad50e52582870ff42
Polina Kostsova <kostsovapa@gmail.com>
2023-10-12 01:56:04 +0300

    Task1

--- Program.cs
+++ Program.cs
@@ -1,8 +1,29 @@
 ﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-int Prompt (string message)
+Console.Write("Введите количество элементов массива:");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] Array = new int[m];
+
+void mas(int m)
 {
-    System.Console.Write (message);
-    string value = Console.ReadLine();
-    int result = Convert.ToInt32(value);
-    return result;
+for (int i = 0; i < m; i++)
+{
+Console.WriteLine("Введите {0} элемент массива", i+1);
+Array[i] = Convert.ToInt32(Console.ReadLine());
 }
+}
+
+int kol(int[] Array)
+{
+int i=0;
+int sum = 0;
+while (i < Array.Length)
+{
+if(Array[i]>0)
+sum = sum + 1;
+i = i + 1;
+}
+return sum;
+}
+
+mas(m);
+Console.Write("\n Чисел больше нуля: {0}", kol(Array));
